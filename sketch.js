3fa7f9ac87ed9f17965a75b4add9ef888143ce0d
003VrajShah <73598108+003VrajShah@users.noreply.github.com>
2020-12-05 10:01:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+hr=hour();
+mn=minute();
+sc=second();
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  scAngle=map(sc,0,60,0,360);
+}
+
+function draw() {
+  background(255,255,255);  
+  angleMode(DEGREES);
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+  drawSprites();
+}
